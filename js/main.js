72cee607e620df2b987d2b2011059d41ca2ff75e
Evgeniy Klimovch <kliasanik@gmail.com>
2022-10-11 15:18:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,6 @@ var num_3 = 5;
 num_3 += 7;
 console.log("Resultat: " + num_3);
 
-var str_1 ="12";
-var str_2 ="2";
+var str_1 = Number("12");
+var str_2 = Number("2");
 console.log("Результат: " + (str_1 + str_2));
