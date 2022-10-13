e2be505a8dd8c0b97645fd36786a58ea12de6b25
Evgeniy Klimovch <kliasanik@gmail.com>
2022-10-13 11:05:11 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,12 +38,13 @@ var stroka ="word";
 
 switch(stroka) {
     case "4":
-        console.log("Переменная со значением 4")
+        console.log("Переменная со значением 4");
         break;
     case "45":
-        console.log("Переменная со значением 45")
+        console.log("Переменная со значением 45");
         break;
     case "word":
-        console.log("Переменная со значением word")
+        console.log("Переменная со значением word");
         break;
+    default.log("Default");
 }
