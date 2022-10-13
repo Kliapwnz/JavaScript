865f856bd1d84cb74ad45ab2144e72727541e1a5
Kliapwnz <kliasanik@gmail.com>
2022-10-13 16:23:58 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,14 +48,6 @@ switch(stroka) {
         break;
 }
 
+for(var i = 0; i < 10; i++) {
 
-
-var arr = [12, true, "KLIA", 5.7, 500, 800];
-console.log(arr.length);
-
-var matrix = [
-    [4 ,6, 8], ["KLIA", 5.7], [500, 800]
-];
-
-matrix[1][0] = "KLIApwnz";
-console.log(matrix);
+}
